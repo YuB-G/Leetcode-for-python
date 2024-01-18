5fd5460d791f500868b77f49d501350e70eb50d7
Bofan Yu <bofan@BofandeMacBook-Pro.local>
2024-01-17 16:17:05 -0800

    few question

---
+++ 249-group-shifted-string.py
@@ -0,0 +1,23 @@
+# Question:
+# We can shift a string by shifting 
+# each of its letters to its successive letter.
+
+# For example, "abc" can be shifted to be "bcd".
+# We can keep shifting the string to form a sequence.
+
+# For example, we can keep shifting "abc" to 
+# form the sequence: "abc" -> "bcd" -> ... -> "xyz".
+# Given an array of strings strings, 
+# group all strings[i] that belong to 
+# the same shifting sequence. 
+# You may return the answer in any order.
+
+# Example 1:
+# Input: strings = ["abc","bcd","acef","xyz","az","ba","a","z"]
+# Output: [["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]]
+
+# Example 2:
+# Input: strings = ["a"]
+# Output: [["a"]]
+
+# Note: 这一题主要关注的是把具有相同shifted类别的array分组
---
+++ hash-table/49-group-anagram.py
@@ -0,0 +1,14 @@
+# Given an array of strings strs, group the anagrams together. 
+# You can return the answer in any order.
+
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Example 2:
+# Input: strs = [""]
+# Output: [[""]]
+
+# Example 3:
+# Input: strs = ["a"]
+# Output: [["a"]]
