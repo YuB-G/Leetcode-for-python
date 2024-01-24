33c6ee3490205fc474286a09d2182ade6d547a51
Bofan Yu <bofan@BofandeMacBook-Pro.local>
2024-01-23 22:08:36 -0800

    three-question

---
+++ two-pointer/e-121-valid-Palindrome.py
@@ -0,0 +1,42 @@
+# A phrase is a palindrome if, 
+# after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+# it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum(): # 去除非字母的字符
+                left+=1
+            while left < right and not s[right].isalnum():
+                right-=1
+            if s[left].lower() != s[right].lower(): #去除大写字母
+                return False
+
+            left +=1
+            right -=1
+            
+        return True
