6e17fc5bd4ad3b6100a46b2bb11269b0f7176652
YuB-G <yubf2018@gmail.com>
2025-01-22 13:52:23 -0800

    347

--- Neetcode75/347_top_k_frequent_elements.py
+++ Neetcode75/347_top_k_frequent_elements.py
@@ -26,6 +26,9 @@ class Solution:
 # if len(heap)>k:
 #     pop
 
+import heapq
+import collections
+from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
